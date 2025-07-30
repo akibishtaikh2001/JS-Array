7cd73e1ef21d818adcb9d2ff13429d4cae661acd
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 00:01:27 +0600

    Array

---
+++ array.js
@@ -0,0 +1,4 @@
+const numbers = [12, 22, 54, 65];
+const friends = ['abul', 'babul', 'cabul', 'dabul', 'bulbul'];
+const ispassed = [true, false, true, false]
+const mixedArray = [12, 'Alahabd', true , 45.555 ];
