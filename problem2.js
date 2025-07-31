283265aaa5532d22460d73d1cdc843a8e6d25cdb
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 14:37:17 +0600

    Task 2 - Add or remove elements

---
+++ problem2.js
@@ -0,0 +1,8 @@
+let toristDestination = ['cox Bazar', 'Bandorban', 'Shundarban',]
+console.log(toristDestination);
+toristDestination.push('kaptai lake');
+console.log(toristDestination);
+toristDestination.push('Sajek', 'rabeya park');
+console.log(toristDestination);
+toristDestination.pop();
+console.log(toristDestination);
