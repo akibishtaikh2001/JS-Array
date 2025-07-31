0bb392153b9fc35cc803c83f4728600c07f0b652
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-31 14:59:39 +0600

    Task 5  - Combining Arrays

---
+++ problem5.js
@@ -0,0 +1,8 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+const nums = [12, 447, 544, 45, 4, 4, 54, 5, 4];
+
+const combaint = friends.concat(nums);
+
+console.log(combaint);
+
